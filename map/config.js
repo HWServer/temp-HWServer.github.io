6a0c34153cba9833511613a24c91d012da079825
Rayawa <171078364@qq.com>
2021-02-09 14:01:48 +0800

    Added map 1

---
+++ map/config.js
@@ -0,0 +1,55 @@
+var CONFIG = {
+  "maps": {
+    "survival": {
+      "imageFormat": "png",
+      "lastRendered": [
+        1612769738,
+        0,
+        0,
+        0
+      ],
+      "maxZoom": 10,
+      "name": "HWS Survival",
+      "renderView": "isometric",
+      "rotations": [
+        0
+      ],
+      "textureSize": 12,
+      "tileSetGroup": "survival_isometric_t1",
+      "tileSize": [
+        384,
+        384
+      ],
+      "world": "survival",
+      "worldName": "survival",
+      "worldSeaLevel": 64
+    }
+  },
+  "mapsOrder": [
+    "survival"
+  ],
+  "tileSetGroups": {
+    "survival_isometric_t1": {
+      "maxZoom": 10,
+      "tileOffsets": [
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ]
+      ],
+      "tileWidth": 1
+    }
+  }
+};
