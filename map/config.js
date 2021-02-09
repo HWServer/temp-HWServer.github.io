7a755a252434c636f29a0d8572e6b583b50224a2
Rayawa <73282963+Rayawa@users.noreply.github.com>
2021-02-09 22:12:28 +0800

    Add map 1&4

---
+++ map/config.js
@@ -0,0 +1,55 @@
+var CONFIG = {
+  "maps": {
+    "HWS_isometric_day": {
+      "imageFormat": "png",
+      "lastRendered": [
+        1612866206,
+        0,
+        0,
+        0
+      ],
+      "maxZoom": 9,
+      "name": "HWS-survival",
+      "renderView": "isometric",
+      "rotations": [
+        0
+      ],
+      "textureSize": 12,
+      "tileSetGroup": "survival_isometric_t1",
+      "tileSize": [
+        384,
+        384
+      ],
+      "world": "survival",
+      "worldName": "survival",
+      "worldSeaLevel": 64
+    }
+  },
+  "mapsOrder": [
+    "HWS_isometric_day"
+  ],
+  "tileSetGroups": {
+    "survival_isometric_t1": {
+      "maxZoom": 9,
+      "tileOffsets": [
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ]
+      ],
+      "tileWidth": 1
+    }
+  }
+};
